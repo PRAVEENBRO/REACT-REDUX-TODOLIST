415ef7da263667a15ddd8fe2cdbbaae69f341f20
PRAVEENBRO <93118431+PRAVEENBRO@users.noreply.github.com>
2022-01-15 20:38:40 +0530

    Add files via upload

--- src/components/TodoInputs.js
+++ src/components/TodoInputs.js
@@ -7,17 +7,13 @@ import { v1 as uuid } from 'uuid'
 const TodoInputs = () => {
 
     const dispatch = useDispatch()
-    const [name, setname] = useState()
+    const [name, setname] = useState('')
 
     return (
         <div className='pt-1'>
             <h1>Todo List</h1>
             <div className='d-flex justify-content-center mx-3'>
-                <Form.Control className='inputform'
-                    placeholder="Add Todos..." 
-                    type="text"
-                    onChange={(e) => setname(e.target.value)}
-                />
+                <Form.Control className='inputform' placeholder="Add Todos..." type="text" onChange={(e) => setname(e.target.value)} value={name} />
 
                 <Button variant="dark" className='mx-2 addbtn' onClick={() => {
                     dispatch(addTodo(
